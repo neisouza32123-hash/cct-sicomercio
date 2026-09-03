f258195f5f7b0b6191b63f4abc5cc25c75aa66cd
neisouza32123-hash <neisouza32123@gmail.com>
2026-09-03 16:51:46 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,45 @@
-const CACHE='cct-sicomercio-25-26-v2';
-const ASSETS=['./','index.html','style.css','app.js','data.js','manifest.webmanifest','icon-192.png','icon-512.png','logo-sicomercio.png'];
+const CACHE="cct-sicomercio-25-26-v4";
+const ASSETS=["./","index.html","style.css","app.js","data.js","manifest.webmanifest","icon-192.png","icon-512.png","logo-sicomercio.png"];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   const req = event.request;
 
-  if (req.mode === 'navigate') {
+  if (req.mode === "navigate") {
     event.respondWith(
       fetch(req)
         .then(resp => {
           const copy = resp.clone();
-          caches.open(CACHE).then(cache => cache.put('./', copy));
+          caches.open(CACHE).then(cache => cache.put("./", copy));
           return resp;
         })
-        .catch(() => caches.match('./'))
+        .catch(() => caches.match("./"))
     );
     return;
   }
 
   event.respondWith(
-    fetch(req)
-      .then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE).then(cache => cache.put(req, copy));
-        return resp;
-      })
-      .catch(() => caches.match(req))
+    caches.match(req).then(cached => {
+      const network = fetch(req)
+        .then(resp => {
+          const copy = resp.clone();
+          caches.open(CACHE).then(cache => cache.put(req, copy));
+          return resp;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
   );
 });
