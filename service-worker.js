63600929034e13567a76bcbd9009358653a10a40
neisouza32123-hash <neisouza32123@gmail.com>
2026-09-03 16:23:35 -0300

    Atualiza service worker para V2 e limpa cache antigo

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,42 @@
-const CACHE='cct-sicomercio-25-26-v1';const ASSETS=['./','index.html','style.css','app.js','data.js','manifest.webmanifest','icon-192.png','icon-512.png','logo-sicomercio.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
+const CACHE='cct-sicomercio-25-26-v2';
+const ASSETS=['./','index.html','style.css','app.js','data.js','manifest.webmanifest','icon-192.png','icon-512.png','logo-sicomercio.png'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req)
+        .then(resp => {
+          const copy = resp.clone();
+          caches.open(CACHE).then(cache => cache.put('./', copy));
+          return resp;
+        })
+        .catch(() => caches.match('./'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(req)
+      .then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(req, copy));
+        return resp;
+      })
+      .catch(() => caches.match(req))
+  );
+});
